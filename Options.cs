9eedb6080c93af9fee60279260ba3c01e165f509
Andrew Armstrong <phplasma@gmail.com>
2012-08-22 17:42:20 +1000

    Initial import

---
+++ Options.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace BlobUtility
+{
+	public class Options : CommandLineOptionsBase
+	{
+		[Option("k", "key", HelpText = "Blob storage Access Key", Required = true)]
+		public string Key { get; set; }
+
+		[Option("a", "account", HelpText = "Blob storage Account Name", Required = true)]
+		public string Account { get; set; }
+
+		[Option("c", "container", HelpText = "Blob storage Container Name", Required = true)]
+		public string Container { get; set; }
+
+		[Option("d", "directory", HelpText = "Destination directory to upload to")]
+		public string Directory { get; set; }
+
+		[OptionArray("f", "files", HelpText = "Specifies the local filenames to upload", Required = true)]
+		public string[] Files { get; set; }
+
+		[Option(null, "brief", DefaultValue = false, HelpText = "Show minimal backup progress log information.")]
+		public bool Brief { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using log4net;
+using log4net.Filter;
+
+namespace BlobUtility
+{
+	class Program
+	{
+		static readonly ILog _log = LogManager.GetLogger(typeof (Program));
+
+		static int Main(string[] args)
+		{
+			// Parse Options
+			var options = new Options();
+			if (!CommandLineParser.Default.ParseArguments(args, options))
+				return 1;
+
+			// Configure Logging
+			ConfigureLogging(options.Brief);
+
+			// Fetch References
+			var credentials = new StorageCredentialsAccountAndKey(options.Account, options.Key);
+			var account = new CloudStorageAccount(credentials, true);
+			var client = account.CreateCloudBlobClient();
+			var container = client.GetContainerReference(options.Container);
+
+			// Resolve Files
+			var files = new List<string>();
+			var workingDirectoryUri = new Uri(string.Format("{0}/", Environment.CurrentDirectory), UriKind.Absolute);
+			foreach(var file in options.Files)
+				files.AddRange(Directory.GetFiles(Environment.CurrentDirectory, file));
+
+			// Perform Upload
+			_log.Info(string.Format("Uploading {0} file(s)", files.Count));
+			foreach(var file in files) {
+				var fileUri = new Uri(file, UriKind.Absolute);
+				var relativePath = workingDirectoryUri.MakeRelativeUri(fileUri);
+				var uploadPath = Path.Combine(options.Directory ?? string.Empty, relativePath.ToString()).Replace("\\", "/");
+				var blob = container.GetBlobReference(uploadPath);
+
+				_log.Info(string.Format("Uploading {0} to {1}", relativePath, uploadPath));
+				blob.UploadFile(file);
+			}
+
+			_log.Info("Finished uploading");
+			return 0;
+		}
+
+		static void ConfigureLogging(bool verbose)
+		{
+			var consoleAppender = new log4net.Appender.ConsoleAppender();
+			consoleAppender.Layout = new log4net.Layout.PatternLayout("[%date{yyyy-MM-dd HH:mm:ss}] %-5p %c{1} - %m%n");
+
+			if (!verbose) {
+				var filter = new LoggerMatchFilter();
+				filter.AcceptOnMatch = true;
+				filter.LoggerToMatch = typeof(Program).ToString();
+				consoleAppender.AddFilter(filter);
+
+				consoleAppender.AddFilter(new DenyAllFilter());
+			}
+
+			log4net.Config.BasicConfigurator.Configure(consoleAppender);
+		}
+	}
+}
